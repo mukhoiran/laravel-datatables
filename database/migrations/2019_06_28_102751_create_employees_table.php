d7f5e5303c29d93f15667e096566c5b31268c8ff
Muhammad Khoirul Anam <mkhoir.anam@gmail.com>
2019-06-28 17:40:29 +0700

    fix migrate error : SQLSTATE[42000] 1071 Specified key was too long

--- database/migrations/2019_06_28_102751_create_employees_table.php
+++ database/migrations/2019_06_28_102751_create_employees_table.php
@@ -15,6 +15,8 @@ class CreateEmployeesTable extends Migration
     {
         Schema::create('employees', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->timestamps();
         });
     }
